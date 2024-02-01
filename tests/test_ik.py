7fb6dc695fd94eaf200cc8a97768aa8981731d40
drenayaz <coraliedeplanne@live.fr>
2024-02-01 17:44:15 +0100

    add unit test

--- tests/test_ik.py
+++ tests/test_ik.py
@@ -2,10 +2,21 @@ from reachy2_symbolic_ik.ik_symbolic import IK_symbolic
 import numpy as np
 from reachy_placo.ik_reachy_placo import IKReachyQP
 from scipy.spatial.transform import Rotation as R
+import time
 
 def test_class() -> None:
     symbolic_ik = IK_symbolic(upper_arm_size=0.28, forearm_size=0.28, gripper_size=0.15)
-    # placo_ik = IKReachyQP()
+    placo_ik = IKReachyQP(
+        viewer_on=True,
+        collision_avoidance=False,
+        parts=["r_arm"],
+        position_weight=1.9,
+        orientation_weight=1e-2,
+        robot_version="reachy_2",
+        velocity_limit=50.0
+    )
+    placo_ik.setup(urdf_path="src/config_files/new_new_reachy2_1.urdf")
+    placo_ik.create_tasks()
     assert symbolic_ik is not None
     
 
@@ -21,7 +32,7 @@ def test_class() -> None:
     assert result[2] is None
 
 
-    goal_position = [0.6, -0.2, -0.0]
+    goal_position = [0.3, -0.2, -0.3]
     goal_orientation = [-0,-90,0]
     goal_orientation = np.deg2rad(goal_orientation)
     goal_pose = [goal_position, goal_orientation]
@@ -29,11 +40,12 @@ def test_class() -> None:
     result = symbolic_ik.is_reachable(goal_pose)
 
     assert result[0]
-    assert result[1] == [0,np.pi*2]
+    assert result[1][0] >= 0
+    assert result[1][1] <= 4*np.pi
     assert result[2] is not None
 
 
-    joints = result[2](0)
+    joints = result[2](result[1][0])
 
     assert len(joints) == 7
 
@@ -53,29 +65,21 @@ def test_class() -> None:
     joints = result[2](0)
 
     assert len(joints) == 7
-    assert np.allclose(joints[0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+    names = ["r_shoulder_pitch", "r_shoulder_roll", "r_elbow_yaw", "r_elbow_pitch", "r_wrist_roll", "r_wrist_pitch", "r_wrist_yaw"]
+    for i in range(len(names)):
+        placo_ik.robot.set_joint(names[i], joints[i])
+    placo_ik._tick_viewer()
+
+    T_torso_tip = placo_ik.robot.get_T_a_b("torso", "r_tip_joint")
+    position = T_torso_tip[:3, 3]
+    orientation = T_torso_tip[:3, :3]
+    orientation = R.from_matrix(orientation).as_euler("xyz")
+    goal_position = goal_pose[0]
+    goal_orientation = goal_pose[1]
+
+    assert np.allclose(position, goal_position)
+    assert np.allclose(orientation, goal_orientation)
 
 
-    # names = ["r_shoulder_pitch", "r_shoulder_roll", "r_elbow_yaw", "r_elbow_pitch", "r_wrist_roll", "r_wrist_pitch", "r_wrist_yaw"]
-    # for i in range(len(names)):
-    #     placo_ik.robot.set_joint(names[i], joints[i])
-    # # placo_ik._tick_viewer()
 
-
-    # T_torso_tip = placo_ik.robot.get_T_a_b("torso", "r_tip_joint")
-    # position = T_torso_tip[:3, 3]
-    # orientation = T_torso_tip[:3, :3]
-    # orientation = R.from_matrix(orientation).as_euler("xyz")
-    # goal_position = goal_pose[0]
-    # goal_orientation = goal_pose[1]
-
-    # assert np.isclose(position, goal_position)
-    # for i in range(3):
-    #     if abs(position[i] - goal_position[i]) > position_tolerance:
-    #         print("Position not correct")
-    #         return False
-    # for i in range(3):
-    #     if abs(orientation[i] - goal_orientation[i]) > orientation_tolerance:
-    #         print("Orientation not correct")
-    #         return False
-    # return True
