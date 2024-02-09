b9ce8ebcfd9d275d694fd877641fc7330cdd6f09
drenayaz <coraliedeplanne@live.fr>
2024-02-09 16:27:38 +0100

    reorganized symbolic ik

---
+++ src/benchmark/ik_benchmarks.py
@@ -0,0 +1,157 @@
+import time
+
+import numpy as np
+
+from reachy2_symbolic_ik.symbolic_ik import SymbolicIK
+from reachy2_symbolic_ik.utils import (
+    make_homogenous_matrix_from_rotation_matrix,
+    rotation_matrix_from_vectors,
+)
+
+
+def test_is_reachable(ik: SymbolicIK) -> None:
+    goal_position = [0.3, -0.1, 0.1]
+    goal_orientation = [np.radians(20), np.radians(-50), np.radians(20)]
+    goal_pose = np.array([goal_position, goal_orientation])
+    start_time = time.time()
+    for i in range(10000):
+        ik.is_reachable(goal_pose)
+    end_time = time.time()
+    print("is_reachable : ", end_time - start_time)
+    pass
+
+
+def test_get_joints(ik: SymbolicIK) -> None:
+    goal_position = [0.3, -0.1, 0.1]
+    goal_orientation = [np.radians(20), np.radians(-50), np.radians(20)]
+    goal_pose = np.array([goal_position, goal_orientation])
+    result = ik.is_reachable(goal_pose)
+    alpha = result[1][0]
+    start_time = time.time()
+    for i in range(10000):
+        ik.get_joints(alpha)
+    end_time = time.time()
+    print("get_joints : ", end_time - start_time)
+
+
+def test_get_wrist_position(ik: SymbolicIK) -> None:
+    goal_position = [0.3, -0.1, 0.1]
+    goal_orientation = [np.radians(20), np.radians(-50), np.radians(20)]
+    goal_pose = np.array([goal_position, goal_orientation])
+    start_time = time.time()
+    for i in range(10000):
+        ik.get_wrist_position(goal_pose)
+    end_time = time.time()
+    print("get_wrist_position : ", end_time - start_time)
+
+
+def test_get_limitation_wrist_circle(ik: SymbolicIK) -> None:
+    goal_position = [0.3, -0.1, 0.1]
+    goal_orientation = [np.radians(20), np.radians(-50), np.radians(20)]
+    goal_pose = np.array([goal_position, goal_orientation])
+    ik.wrist_position = ik.get_wrist_position(goal_pose)
+    start_time = time.time()
+    for i in range(10000):
+        ik.get_limitation_wrist_circle(goal_pose)
+    end_time = time.time()
+    print("get_limitation_wrist_circle : ", end_time - start_time)
+    pass
+
+
+def test_get_intersection_circle(ik: SymbolicIK) -> None:
+    goal_position = [0.3, -0.1, 0.1]
+    goal_orientation = [np.radians(20), np.radians(-50), np.radians(20)]
+    goal_pose = np.array([goal_position, goal_orientation])
+    ik.wrist_position = ik.get_wrist_position(goal_pose)
+    start_time = time.time()
+    for i in range(10000):
+        ik.get_intersection_circle(goal_pose)
+    end_time = time.time()
+    print("get_intersection_circle : ", end_time - start_time)
+    pass
+
+
+def test_are_cricles_linked(ik: SymbolicIK) -> None:
+    goal_position = [0.3, -0.1, 0.1]
+    goal_orientation = [np.radians(20), np.radians(-50), np.radians(20)]
+    goal_pose = np.array([goal_position, goal_orientation])
+    ik.wrist_position = ik.get_wrist_position(goal_pose)
+    intersection_circle = ik.get_intersection_circle(goal_pose)
+    limitation_wrist_circle = ik.get_limitation_wrist_circle(goal_pose)
+    start_time = time.time()
+    for i in range(10000):
+        ik.are_circles_linked(intersection_circle, limitation_wrist_circle)
+    end_time = time.time()
+    print("are_circles_linked : ", end_time - start_time)
+
+
+def test_rotation_matrix_from_vectors() -> None:
+    goal_position = [0.3, -0.1, 0.1]
+    goal_orientation = [np.radians(20), np.radians(-50), np.radians(20)]
+    goal_pose = np.array([goal_position, goal_orientation])
+    ik.wrist_position = ik.get_wrist_position(goal_pose)
+    limitation_wrist_circle = ik.get_limitation_wrist_circle(goal_pose)
+    start_time = time.time()
+    for i in range(10000):
+        rotation_matrix_from_vectors(np.array([1, 0, 0]), limitation_wrist_circle[2])
+    end_time = time.time()
+    print("rotation_matrix_from_vectors : ", end_time - start_time)
+
+
+def test_points_of_nearest_point(ik: SymbolicIK) -> None:
+    goal_position = [0.3, -0.1, 0.1]
+    goal_orientation = [np.radians(20), np.radians(-50), np.radians(20)]
+    goal_pose = np.array([goal_position, goal_orientation])
+    ik.wrist_position = ik.get_wrist_position(goal_pose)
+    intersection_circle = ik.get_intersection_circle(goal_pose)
+    limitation_wrist_circle = ik.get_limitation_wrist_circle(goal_pose)
+    start_time = time.time()
+    for i in range(10000):
+        ik.points_of_nearest_approach(
+            limitation_wrist_circle[0], limitation_wrist_circle[2], intersection_circle[0], intersection_circle[2]
+        )
+    end_time = time.time()
+    print("points_of_nearest_approach", end_time - start_time)
+
+
+def test_intersection_circle_line_3d(ik: SymbolicIK) -> None:
+    goal_position = [0.3, -0.1, 0.1]
+    goal_orientation = [np.radians(20), np.radians(-50), np.radians(20)]
+    goal_pose = np.array([goal_position, goal_orientation])
+    ik.wrist_position = ik.get_wrist_position(goal_pose)
+    intersection_circle = ik.get_intersection_circle(goal_pose)
+    limitation_wrist_circle = ik.get_limitation_wrist_circle(goal_pose)
+    q, v = ik.points_of_nearest_approach(
+        limitation_wrist_circle[0], limitation_wrist_circle[2], intersection_circle[0], intersection_circle[2]
+    )
+
+    start_time = time.time()
+    for i in range(10000):
+        ik.intersection_circle_line_3d_vd(limitation_wrist_circle[0], limitation_wrist_circle[1], v, q)
+    end_time = time.time()
+    print("intersection_circle_line_3d_vd : ", end_time - start_time)
+
+
+def test_make_homogenous_matrix_from_rotation_matrix() -> None:
+    position = np.array([0.3, -0.1, 0.1])
+    rotation_matrix = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    start_time = time.time()
+    for i in range(10000):
+        make_homogenous_matrix_from_rotation_matrix(position, rotation_matrix)
+    end_time = time.time()
+    print("make_homogenous_matrix_from_rotation_matrix : ", end_time - start_time)
+
+
+if __name__ == "__main__":
+    ik = SymbolicIK()
+    test_is_reachable(ik)
+    test_get_joints(ik)
+    test_get_wrist_position(ik)
+    test_get_limitation_wrist_circle(ik)
+    test_get_intersection_circle(ik)
+    test_are_cricles_linked(ik)
+    test_rotation_matrix_from_vectors()
+    test_points_of_nearest_point(ik)
+    test_intersection_circle_line_3d(ik)
+    test_make_homogenous_matrix_from_rotation_matrix()
+    print("All tests passed!")
--- src/reachy2_symbolic_ik/utils.py
+++ src/reachy2_symbolic_ik/utils.py
@@ -1,11 +1,9 @@
 import math
 import time
-from typing import List
+from typing import Any, List
 
 import numpy as np
 import numpy.typing as npt
-
-# import numpy as np
 from reachy_placo.ik_reachy_placo import IKReachyQP
 
 
@@ -37,3 +35,85 @@ def r_arm_joint_names() -> List[str]:
     names.append("r_wrist_pitch")
     names.append("r_wrist_yaw")
     return names
+
+
+def make_homogenous_matrix_from_rotation_matrix(
+    position: npt.NDArray[np.float64], rotation_matrix: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return np.array(
+        [
+            [rotation_matrix[0][0], rotation_matrix[0][1], rotation_matrix[0][2], position[0]],
+            [rotation_matrix[1][0], rotation_matrix[1][1], rotation_matrix[1][2], position[1]],
+            [rotation_matrix[2][0], rotation_matrix[2][1], rotation_matrix[2][2], position[2]],
+            [0.0, 0.0, 0.0, 1.0],
+        ]
+    )
+
+
+def rotation_matrix_from_vectors(vect1: npt.NDArray[np.float64], vect2: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    """Find the rotation matrix that aligns vect1 to vect2
+    :param vect1: A 3d "source" vector
+    :param vect2: A 3d "destination" vector
+    :return mat: A transform matrix (3x3) which when applied to vect1, aligns it with vect2.
+    """
+    if np.all(np.isclose(vect1, vect2)):
+        return np.eye(3)
+    a, b = (vect1 / np.linalg.norm(vect1)).reshape(3), (vect2 / np.linalg.norm(vect2)).reshape(3)
+    v = np.cross(a, b)
+    c = np.dot(a, b)
+    s = np.linalg.norm(v)
+    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+    rotation_matrix = np.array(np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s**2)))
+    return rotation_matrix
+
+
+def show_point(ax: Any, point: npt.NDArray[np.float64], color: str) -> None:
+    ax.plot(point[0], point[1], point[2], "o", color=color)
+
+
+def show_circle(
+    ax: Any,
+    center: npt.NDArray[np.float64],
+    radius: float,
+    normal_vector: npt.NDArray[np.float64],
+    intervalles: npt.NDArray[np.float64],
+    color: str,
+) -> None:
+    theta = []
+    for intervalle in intervalles:
+        angle = np.linspace(intervalle[0], intervalle[1], 100)
+        for a in angle:
+            theta.append(a)
+
+    y = radius * np.cos(theta)
+    z = radius * np.sin(theta)
+    x = np.zeros(len(theta))
+    Rmat = rotation_matrix_from_vectors(np.array([1.0, 0.0, 0.0]), np.array(normal_vector))
+    Tmat = np.array(
+        [
+            [Rmat[0][0], Rmat[0][1], Rmat[0][2], center[0]],
+            [Rmat[1][0], Rmat[1][1], Rmat[1][2], center[1]],
+            [Rmat[2][0], Rmat[2][1], Rmat[2][2], center[2]],
+            [0, 0, 0, 1],
+        ]
+    )
+
+    x2 = np.zeros(len(theta))
+    y2 = np.zeros(len(theta))
+    z2 = np.zeros(len(theta))
+    for k in range(len(theta)):
+        p = [x[k], y[k], z[k], 1]
+        p2 = np.dot(Tmat, p)
+        x2[k] = p2[0]
+        y2[k] = p2[1]
+        z2[k] = p2[2]
+    ax.plot(center[0], center[1], center[2], "o", color=color)
+    ax.plot(x2, y2, z2, color)
+
+
+def show_sphere(ax: Any, center: npt.NDArray[np.float64], radius: np.float64, color: str) -> None:
+    u, v = np.mgrid[0 : 2 * np.pi : 30j, 0 : np.pi : 20j]  # type: ignore
+    x = np.cos(u) * np.sin(v) * radius + center[0]
+    y = np.sin(u) * np.sin(v) * radius + center[1]
+    z = np.cos(v) * radius + center[2]
+    ax.plot_wireframe(x, y, z, color=color, alpha=0.2)
