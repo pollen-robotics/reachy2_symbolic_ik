14694d820c8cbf53e4c35fa66b57fabd61317952
drenayaz <coraliedeplanne@live.fr>
2024-02-26 11:17:19 +0100

    fixed out of range edge case

--- src/reachy2_symbolic_ik/symbolic_ik.py
+++ src/reachy2_symbolic_ik/symbolic_ik.py
@@ -87,21 +87,22 @@ class SymbolicIK:
         self.wrist_position = self.get_wrist_position(goal_pose)
 
         d_shoulder_wrist = np.linalg.norm(self.wrist_position - self.shoulder_position)
-        # if d_shoulder_wrist > self.upper_arm_size + self.forearm_size:
-        #     # todo check if the pose is the sphere of the arm
-        #     # todo check Trex arm
-        #     return False, np.array([]), None
+        
+        # Test if the wrist is in the arm range
+        if d_shoulder_wrist > self.upper_arm_size + self.forearm_size:
+            # todo check if the pose is the sphere of the arm
+            # todo check Trex arm
+            return False, np.array([]), None
 
         # TODO these values very often are not in the limits, is it normal? Is clipping a good idea?
-        to_asin1 = np.clip(d_shoulder_wrist / (2 * self.upper_arm_size), -1, 1)
-        to_asin2 = np.clip(d_shoulder_wrist / (2 * self.forearm_size), -1, 1)
+        # to_asin1 = np.clip(d_shoulder_wrist / (2 * self.upper_arm_size), -1, 1)
+        # to_asin2 = np.clip(d_shoulder_wrist / (2 * self.forearm_size), -1, 1)
+        
+        to_asin1 = d_shoulder_wrist / (2 * self.upper_arm_size)
+        to_asin2 = d_shoulder_wrist / (2 * self.forearm_size)
         print(f"to_asin1: {to_asin1}")
         print(f"to_asin2: {to_asin2}")
-        # #limit the asin to -1, 1
-        # if to_asin1 > 1:
-        #     to_asin1 = 1
-        # if to_asin1 < -1:
-        #     to_asin1 = -1
+        
         alpha = np.arcsin(to_asin1) + np.arcsin(to_asin2) - np.pi
         if alpha < np.radians(-self.elbow_limits) or alpha > np.radians(self.elbow_limits):
             return False, np.array([]), None
